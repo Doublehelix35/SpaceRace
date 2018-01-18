3fdbc2a85d9aebdd6fcdfb98671759ec9390d546
Mitchell Dixon <mitchelldixon35@gmail.com>
2018-01-18 18:10:47 +0000

    Pac monster moving
    Pac-monster moves back and forth

--- SpaceRaceGame/Assets/Scripts/Bosses/S_PacMonster.cs
+++ SpaceRaceGame/Assets/Scripts/Bosses/S_PacMonster.cs
@@ -4,13 +4,46 @@ using UnityEngine;
 
 public class S_PacMonster : MonoBehaviour {
 
+    public Transform Waypoint01;
+    public Transform Waypoint02;
+
+    public Vector3 Scale = new Vector3(2f,2f,2f);
+    public float Speed = 1f;
+    bool FaceRight = true;
+
+    Vector2 dir = Vector2.right;
+
+
 	// Use this for initialization
 	void Start () {
-		
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        
+
+		if(transform.position.x < Waypoint01.position.x)
+        {
+            dir = Vector2.right;
+            FaceRight = true;
+        }
+        else if (transform.position.x > Waypoint02.position.x)
+        {
+            dir = Vector2.left;
+            FaceRight = false;
+        }
+
+        if (FaceRight)
+        {
+            transform.localScale = new Vector3(Scale.x, Scale.y, Scale.z);
+        }
+        else
+        {
+            transform.localScale = new Vector3(-Scale.x, Scale.y, Scale.z);
+        }
+
+        transform.Translate(dir * Speed * Time.deltaTime);
+    }
 }

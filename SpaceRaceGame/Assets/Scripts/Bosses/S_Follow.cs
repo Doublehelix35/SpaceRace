c65823d2c7af3c017fdeee272baedac85832fc34
Mitchell Dixon <mitchelldixon35@gmail.com>
2018-01-19 14:58:09 +0000

    Ghosts update
    Ghosts in game and following boss. Phase 1 nearly complete

---
+++ SpaceRaceGame/Assets/Scripts/Bosses/S_Follow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_Follow : MonoBehaviour {
+
+    public Transform TransformToFollow;
+    public float DistanceToFollowAt;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(TransformToFollow.position.x + DistanceToFollowAt, TransformToFollow.position.y, transform.position.z);
+	}
+}

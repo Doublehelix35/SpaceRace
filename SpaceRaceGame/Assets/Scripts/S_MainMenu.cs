eb51939b4072fc6401e6e7cafc4df03aef1ce1ae
Mitchell Dixon <mitchelldixon35@gmail.com>
2018-01-24 12:33:21 +0000

    Main menu update
    Script starting to come together

--- SpaceRaceGame/Assets/Scripts/S_MainMenu.cs
+++ SpaceRaceGame/Assets/Scripts/S_MainMenu.cs
@@ -7,13 +7,19 @@ public class S_MainMenu : MonoBehaviour {
 
     public GameObject[] Spawners;
     public GameObject[] Bosses;
+    GameObject CurrentBoss; // Boss to move and there should only be one at a time
+    float SpawnTime;
     public Text Title;
     float SizeToScale = 0.2f;
+    float Speed = 1f; // Speed to move bosses at
+
+    int SpawnerNum = 0; // What spawner its on
+    int BossNum = 0; // What boss its on
 
 	
 	void Start ()
     {
-
+        SpawnBoss();
 	}
 	
 	void Update ()
@@ -25,8 +31,36 @@ public class S_MainMenu : MonoBehaviour {
 
     }
 
-    void SpawnBoss(int SpawnerNum, int BossNum)
+    void SpawnBoss()
     {
-        GameObject Boss = Instantiate(Bosses[BossNum], Spawners[SpawnerNum].transform.position, Spawners[SpawnerNum].transform.rotation);
+        CurrentBoss = Instantiate(Bosses[BossNum], Spawners[SpawnerNum].transform.position, Spawners[SpawnerNum].transform.rotation);
+        SpawnTime = Time.time;
+
+        StartCoroutine("MoveThenKill");
+
+        SpawnerNum++;
+        BossNum++;
+
+        if (SpawnerNum > Spawners.Length) // Loop back around
+        {
+            SpawnerNum = 0;
+        }
+
+        if (BossNum > Bosses.Length) // Loop back around
+        {
+            BossNum = 0;
+        }
+    }
+
+    IEnumerator MoveThenKill()
+    {
+        while (SpawnTime + 4f > Time.time)
+        {
+            CurrentBoss.transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+        }        
+
+        yield return new WaitForSeconds(4f);
+
+        CurrentBoss = null;
     }
 }

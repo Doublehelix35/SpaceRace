600bd6d4e669d983bdfe76f6998ec6b507b4330c
Mitchell Dixon <mitchelldixon35@gmail.com>
2018-01-24 19:23:52 +0000

    New Folders and level select update
    Added new folder for organisation
    Level select(Asteroid) is complete

--- SpaceRaceGame/Assets/Scripts/Menus/S_LevelSelect.cs
+++ SpaceRaceGame/Assets/Scripts/Menus/S_LevelSelect.cs
@@ -5,12 +5,23 @@ using UnityEngine.UI;
 
 public class S_LevelSelect : MonoBehaviour {
 
+    public Image AsteroidImage;
+    public Image SnakeImage;
+    public Image PacMonsterImage;
     public Text Title;
     float SizeToScale = 0.2f;
 
+    public Sprite[] AsteroidSprites;
+    public Sprite[] SnakeSprites;
+    public Sprite[] PacMonsterSprites;
+
+    float ImageRefreshTimer = 2f;
+    float LastImageRefresh; // Time.time of last refresh
+    int ImageCount = 0; // What image to show
+
     void Start ()
     {
-		
+        LastImageRefresh = Time.time;	
 	}
 	
 	void Update ()
@@ -19,5 +30,19 @@ public class S_LevelSelect : MonoBehaviour {
                                         Mathf.PingPong(Time.time / 6, SizeToScale) + 1f,
                                         0f);
         Title.rectTransform.localScale = ScaledVec;
+
+        if(LastImageRefresh + 2f < Time.time)
+        {
+            // Load new sprites into images
+            AsteroidImage.sprite = AsteroidSprites[ImageCount];
+
+            LastImageRefresh = Time.time;
+            ImageCount++;
+            if(ImageCount >= AsteroidSprites.Length)
+            {
+                // Loop back
+                ImageCount = 0;
+            }
+        }
     }
 }
